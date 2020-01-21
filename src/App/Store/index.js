0789a074272ce1238d8e4514ff4f8bba68b1d35b
Milin Paul <milin.paul@experionglobal.com>
2020-01-21 17:41:47 +0530

    added dev tools for development only

--- src/App/Store/index.js
+++ src/App/Store/index.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly'
 
 import RootReducers from './RootReducer'
 
