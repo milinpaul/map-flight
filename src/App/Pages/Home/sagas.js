25569aa9be61c5bd279e3a5db1110a1930b763c7
Milin Paul <milin.paul@experionglobal.com>
2020-01-22 15:48:01 +0530

    change the url to .env and added an example .env

--- src/App/Pages/Home/sagas.js
+++ src/App/Pages/Home/sagas.js
@@ -7,10 +7,12 @@ import {
   GET_USER_DATA_FAIL
 } from './dux'
 
+const apiUrl = process.env.REACT_APP_API_URL
+
 export function* fetchUserData() {
   try {
     yield put({ type: GET_USER_DATA_START })
-    const response = yield call(fetch, 'https://jsonplaceholder.typicode.com/users')
+    const response = yield call(fetch, apiUrl)
     const data = yield response.json()
     yield put({ type: GET_USER_DATA_SUCCESS, payload: data })
   } catch (error) {
