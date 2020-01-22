f3a2f42008901c36f9d2165ffcc8d8405b61d8c7
Milin Paul <milin.paul@experionglobal.com>
2020-01-22 15:19:05 +0530

    added some comments for easiness in dux

--- src/App/Pages/Home/dux.js
+++ src/App/Pages/Home/dux.js
@@ -1,16 +1,24 @@
+/* ------ ACTION TYPES GOES HERE ------ */
+
 export const GET_USER_DATA_REQUESTED = 'HOME/GET_USER_DATA_REQUESTED'
 export const GET_USER_DATA_START = 'HOME/GET_USER_DATA_START'
 export const GET_USER_DATA_SUCCESS = 'HOME/GET_USER_DATA_SUCCESS'
 export const GET_USER_DATA_FAIL = 'HOME/GET_USER_DATA_FAIL'
 
+/* ------ ACTION CREATORS GOES HERE ------ */
+
 export const getUserDataRequest = () => ({
   type: GET_USER_DATA_REQUESTED
 })
+
+/* ------ INITIAL STATE GOES HERE ------ */
 
 const initialState = {
   data: [],
   loading: false
 }
+
+/* ------ DEFAULT REDUCER GOES HERE ------ */
 
 export default (state = initialState, { type, payload }) => {
   switch (type) {
